220482dee5881855dd150baf84119465c5e807d2
Utsavladia <utsavladia@gmail.com>
2024-06-26 23:52:03 +0530

    fixed responsive issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Prediction from './components/Prediction';
-import DataInfo from './components/DataInfo';
-import Visualization from './components/Visualization';
-import Footer from './components/Footer';
-import ContactUs from './components/contact';
-import FloatBtn from './components/FloatBtn';
-import FAQ from './components/FAQ';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Prediction from "./components/Prediction";
+import DataInfo from "./components/DataInfo";
+import Visualization from "./components/Visualization";
+import Footer from "./components/Footer";
+import ContactUs from "./components/contact";
+import FloatBtn from "./components/FloatBtn";
+import FAQ from "./components/FAQ";
 
 const App = () => {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col w-screen">
         <Navbar />
-        <div className="flex-grow">
+        <div className="flex">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/data-info" element={<DataInfo />} />
